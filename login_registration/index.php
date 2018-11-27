bda28191ccdc989b893ac8c73a3f8696ab34e82e
Amadeu Prazeres Pereira <up201605646@gnomo.fe.up.pt>
2018-11-27 12:30:40 +0000

    login and register popup overlay

---
+++ login_registration/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>LTW</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+    <?php include('register.php'); ?>
+    <?php include('login.php'); ?>
+</body>
+</html>
---
+++ login_registration/register.php
@@ -0,0 +1,14 @@
+<a href="#register-popup">Register</a>
+
+<div id="register-popup" class="overlay">
+	<div class="popup">
+		<h2>Register</h2>
+        <a href="#" class="close">&times;</a>
+        <form action="#" method="post">
+            <label>Username:<input type="text" name="username"></label>
+            <label>E-mail:<input type="email" name="email"></label>
+            <label>Password:<input type="password" name="password"></label>
+            <input type="submit" value="Register">
+        </form>
+	</div>
+</div>
