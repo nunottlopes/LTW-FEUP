9b4cebdb743ffc2789a89e735c1905438133d970
Nuno Lopes <up201605337@fe.up.pt>
2018-11-27 12:30:49 +0000

    main page

---
+++ feup_books/templates/common/body.php
@@ -0,0 +1,51 @@
+<div class="geral_div">
+
+    <div class="story">
+        <p>Posted by Amadeu 4 hours ago</p>
+        <h1>Title</h1>
+        <p>Husbands ask repeated resolved but laughter debating. She end cordial visitor noisier fat subject general picture. Or if offering confined entrance no. Nay rapturous him see something residence. Highly talked do so vulgar. Her use behaved spirits and natural attempt say feeling. Exquisite mr incommode immediate he something ourselves it of. Law conduct yet chiefly beloved examine village proceed. </p>
+        <div class="like_comment_share_buttons_story">
+            <button style='font-size:12px'><i class='fas fa-arrow-up'></i> Upvote</button>
+            <button style='font-size:12px'><i class='fas fa-arrow-down'></i> Downvote</button>
+            <button style="font-size:12px"><i class="fa fa-comment"></i> Comments</button>
+            <button style="font-size:12px"><i class="fa fa-share-alt"></i> Share</button>
+        </div>
+    </div>
+
+    <div class="story">
+        <p>Posted by Amadeu 4 hours ago</p>
+        <h1>Title</h1>
+        <p>Husbands ask repeated resolved but laughter debating. She end cordial visitor noisier fat subject general picture. Or if offering confined entrance no. Nay rapturous him see something residence. Highly talked do so vulgar. Her use behaved spirits and natural attempt say feeling. Exquisite mr incommode immediate he something ourselves it of. Law conduct yet chiefly beloved examine village proceed. </p>
+        <div class="like_comment_share_buttons_story">
+            <button style='font-size:12px'><i class='fas fa-arrow-up'></i> Upvote</button>
+            <button style='font-size:12px'><i class='fas fa-arrow-down'></i> Downvote</button>
+            <button style="font-size:12px"><i class="fa fa-comment"></i> Comments</button>
+            <button style="font-size:12px"><i class="fa fa-share-alt"></i> Share</button>
+        </div>
+    </div>
+
+    <div class="story">
+        <p>Posted by Amadeu 4 hours ago</p>
+        <h1>Title</h1>
+        <p>Husbands ask repeated resolved but laughter debating. She end cordial visitor noisier fat subject general picture. Or if offering confined entrance no. Nay rapturous him see something residence. Highly talked do so vulgar. Her use behaved spirits and natural attempt say feeling. Exquisite mr incommode immediate he something ourselves it of. Law conduct yet chiefly beloved examine village proceed. </p>
+        <div class="like_comment_share_buttons_story">
+            <button style='font-size:12px'><i class='fas fa-arrow-up'></i> Upvote</button>
+            <button style='font-size:12px'><i class='fas fa-arrow-down'></i> Downvote</button>
+            <button style="font-size:12px"><i class="fa fa-comment"></i> Comments</button>
+            <button style="font-size:12px"><i class="fa fa-share-alt"></i> Share</button>
+        </div>
+    </div>
+
+    <div class="story">
+        <p>Posted by Amadeu 4 hours ago</p>
+        <h1>Title</h1>
+        <p>Husbands ask repeated resolved but laughter debating. She end cordial visitor noisier fat subject general picture. Or if offering confined entrance no. Nay rapturous him see something residence. Highly talked do so vulgar. Her use behaved spirits and natural attempt say feeling. Exquisite mr incommode immediate he something ourselves it of. Law conduct yet chiefly beloved examine village proceed. </p>
+        <div class="like_comment_share_buttons_story">
+            <button style='font-size:12px'><i class='fas fa-arrow-up'></i> Upvote</button>
+            <button style='font-size:12px'><i class='fas fa-arrow-down'></i> Downvote</button>
+            <button style="font-size:12px"><i class="fa fa-comment"></i> Comments</button>
+            <button style="font-size:12px"><i class="fa fa-share-alt"></i> Share</button>
+        </div>
+    </div>
+
+</div>
