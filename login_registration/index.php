41d089e893dbe1f87bbc24139304cd7cdc63a1b9
Amadeu Prazeres Pereira <up201605646@gnomo.fe.up.pt>
2018-12-03 18:17:44 +0000

    user menu dropdown

--- login_registration/index.php
+++ login_registration/index.php
@@ -5,10 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="style.css" rel="stylesheet">
+    <link href="login.css" rel="stylesheet">
+    <link href="user.css" rel="stylesheet">
 </head>
 <body>
     <?php include('register.php'); ?>
     <?php include('login.php'); ?>
+    <?php include('user.php'); ?>
 </body>
 </html>
