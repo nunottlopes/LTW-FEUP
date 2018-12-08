234d9de273a0c42fc1ad01fe7961d9d734cc4e11
Amadeu Pereira <amadeu.dx1@gmail.com>
2018-12-08 02:07:32 +0000

    css work

--- feup_books/templates/pages/post_page.php
+++ feup_books/templates/pages/post_page.php
@@ -1,57 +1,64 @@
-<div class="view_post">
+<div id="view_post">
 
-    <div class="view_post_container">
-        <article class="">
+    <div id="post">
+        <article class="post_complete">
             <header>Posted by Amadeu 4 hours ago</header>
             <h1>Title</h1>
             <p>Husbands ask repeated resolved but laughter debating. She end cordial visitor noisier fat subject general picture. Or if offering confined entrance no. Nay rapturous him see something residence. Highly talked do so vulgar. Her use behaved spirits and natural attempt say feeling. Exquisite mr incommode immediate he something ourselves it of. Law conduct yet chiefly beloved examine village proceed. </p>
 
-            <footer class="like_comment_share_buttons_post">
+            <footer>
                 <button class="post_button" onclick="upvote()"><i class='fas fa-arrow-up'></i> Upvote</button>
                 <button class="post_button" onclick="downvote()"><i class='fas fa-arrow-down'></i> Downvote</button>
-                <button class="post_button" onclick="comments()"><i class="fa fa-comment"></i> Comments</button>
                 <button class="post_button" onclick="save()"><i class="fa fa-bookmark"></i> Save</button>
                 <button class="post_button" onclick="share()"><i class="fa fa-share-alt"></i> Share</button>
             </footer>
         </article>
 
-        <hr class="hr_divisor">
-
-        <section class="comments_inside_story">
-            <a href="index.php"><span>Username</span></a><span> 15 hours ago</span>
-            <p>Comment here! Merry alone do it burst me songs. Sorry equal charm joy her those folly ham. In they no is many both. Recommend new contented intention improving bed performed age. Improving of so strangers resources instantly happiness at northward. Danger nearer length oppose really add now either. But ask regret eat branch fat garden. Become am he except wishes. Past so at door we walk want such sang. Feeling colonel get her garrets own.</p>
-            <div class="like_comment_share_buttons_story">
-                <button class="comment_button" onclick="upvote()"><i class='fas fa-arrow-up'></i> Upvote</button>
-                <button class="comment_button" onclick="downvote()"><i class='fas fa-arrow-down'></i> Downvote</button>
-                <button class="comment_button" onclick="comments()"><i class="fa fa-comment"></i> Comment</button>
-                <button class="comment_button" onclick="save()"><i class="fa fa-bookmark"></i> Save</button>
-                <button class="comment_button" onclick="share()"><i class="fa fa-share-alt"></i> Share</button>
-            </div>
-
-            <hr class="hr_divisor_comments">
-
-            <a href="index.php"><span>Username</span></a><span> 1 day ago</span>
-            <p>Believing neglected so so allowance existence departure in. In design active temper be uneasy. Thirty for remove plenty regard you summer though. He preference connection astonished on of ye. Partiality on or continuing in particular principles as. Do believing oh disposing to supported allowance we.</p>
-            <div class="like_comment_share_buttons_story">
-                <button class="comment_button" onclick="upvote()"><i class='fas fa-arrow-up'></i> Upvote</button>
-                <button class="comment_button" onclick="downvote()"><i class='fas fa-arrow-down'></i> Downvote</button>
-                <button class="comment_button" onclick="comments()"><i class="fa fa-comment"></i> Comment</button>
-                <button class="comment_button" onclick="save()"><i class="fa fa-bookmark"></i> Save</button>
-                <button class="comment_button" onclick="share()"><i class="fa fa-share-alt"></i> Share</button>
-            </div>
-            <div class="inside_comments">
-                <a href="index.php"><span>Username</span></a><span> 1 day ago</span>
-                <p>Believing neglected so so allowance existence departure in. In design active temper be uneasy. Thirty for remove plenty regard you summer though. He preference connection astonished on of ye. Partiality on or continuing in particular principles as. Do believing oh disposing to supported allowance we.</p>
-                <div class="like_comment_share_buttons_story">
+        <section id="post_comments">
+            <article class="post_comment">
+                <header><a href="profile.php">Username</a> 14 day ago</header>
+                <p>Comment here! Merry alone do it burst me songs. Sorry equal charm joy her those folly ham. In they no is many both. Recommend new contented intention improving bed performed age. Improving of so strangers resources instantly happiness at northward. Danger nearer length oppose really add now either. But ask regret eat branch fat garden. Become am he except wishes. Past so at door we walk want such sang. Feeling colonel get her garrets own.</p>
+                <footer>
                     <button class="comment_button" onclick="upvote()"><i class='fas fa-arrow-up'></i> Upvote</button>
                     <button class="comment_button" onclick="downvote()"><i class='fas fa-arrow-down'></i> Downvote</button>
-                    <button class="comment_button" onclick="comments()"><i class="fa fa-comment"></i> Comment</button>
                     <button class="comment_button" onclick="save()"><i class="fa fa-bookmark"></i> Save</button>
-                    <button class="comment_button" onclick="share()"><i class="fa fa-share-alt"></i> Share</button>
-                </div>
-            </div>
+                </footer>
+            </article>
+
+            <article class="post_comment">
+                <header><a href="profile.php">Username</a> 14 day ago</header>
+                <p>Comment here! Merry alone do it burst me songs. Sorry equal charm joy her those folly ham. In they no is many both. Recommend new contented intention improving bed performed age. Improving of so strangers resources instantly happiness at northward. Danger nearer length oppose really add now either. But ask regret eat branch fat garden. Become am he except wishes. Past so at door we walk want such sang. Feeling colonel get her garrets own.</p>
+                <footer>
+                    <button class="comment_button" onclick="upvote()"><i class='fas fa-arrow-up'></i> Upvote</button>
+                    <button class="comment_button" onclick="downvote()"><i class='fas fa-arrow-down'></i> Downvote</button>
+                    <button class="comment_button" onclick="save()"><i class="fa fa-bookmark"></i> Save</button>
+                </footer>
+            </article>
+
+            <article class="post_comment">
+                <header><a href="profile.php">Username</a> 14 day ago</header>
+                <p>Comment here! Merry alone do it burst me songs. Sorry equal charm joy her those folly ham. In they no is many both. Recommend new contented intention improving bed performed age. Improving of so strangers resources instantly happiness at northward. Danger nearer length oppose really add now either. But ask regret eat branch fat garden. Become am he except wishes. Past so at door we walk want such sang. Feeling colonel get her garrets own.</p>
+                <footer>
+                    <button class="comment_button" onclick="upvote()"><i class='fas fa-arrow-up'></i> Upvote</button>
+                    <button class="comment_button" onclick="downvote()"><i class='fas fa-arrow-down'></i> Downvote</button>
+                    <button class="comment_button" onclick="save()"><i class="fa fa-bookmark"></i> Save</button>
+                </footer>
+            </article>
             
         </section>
+
+    </div>
+
+    <div id="post_page_aside">
+        <div id="channel_subscription" class="aside_div">
+            <h1>Channel</h1>
+            <h2>123123 Subscribers</h1>
+            <a href="#"><button type="button">Subscribe</button></a>
+        </div>
+
+        <div id="aside_footer" class="aside_div">
+            <?php include('templates/common/footer.php') ?>
+        </div>
 
     </div>
 </div>
